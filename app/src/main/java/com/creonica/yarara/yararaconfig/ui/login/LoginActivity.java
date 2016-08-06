01d9ba20cb45e1ef792803257f63c863d49ca9ec
b044113 <marcesantos@gmail.com>
2016-08-06 19:02:08 -0300

    Version 0.0

---
+++ app/src/main/java/com/creonica/yarara/yararaconfig/ui/login/LoginActivity.java
@@ -0,0 +1,210 @@
+package com.creonica.yarara.yararaconfig.ui.login;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.creonica.yarara.yararaconfig.MainActivity;
+import com.creonica.yarara.yararaconfig.R;
+import com.creonica.yarara.yararaconfig.utils.Constants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends Activity {
+
+    private static final String LOG_TAG = LoginActivity.class.getSimpleName();
+    /* A dialog that is presented until the Firebase authentication finished. */
+    private ProgressDialog mAuthProgressDialog;
+
+    /* References to the Auth */
+    private FirebaseAuth mAuth;
+
+    /* Listener for Firebase session changes */
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private EditText mEditTextEmail, mEditTextPassword;
+
+    private SharedPreferences mSharedPref;
+    private SharedPreferences.Editor mSharedPrefEditor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mSharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        mSharedPrefEditor = mSharedPref.edit();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(LOG_TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(LOG_TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        /**
+         * Link layout elements from XML and setup progress dialog
+         */
+        initializeScreen();
+
+        /**
+         * Call signInPassword() when user taps "Done" keyboard action
+         */
+        mEditTextPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+
+                if (actionId == EditorInfo.IME_ACTION_DONE || keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                    signInPassword();
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        /* Add auth listener to Firebase ref */
+        mAuth.addAuthStateListener(mAuthStateListener);
+
+        /**
+         * Get the newly registered user email if present, use null as default value
+         */
+        String signupEmail = mSharedPref.getString(Constants.KEY_SIGNUP_EMAIL, null);
+
+        /**
+         * Fill in the email editText and remove value from SharedPreferences if email is present
+         */
+        if (signupEmail != null) {
+            mEditTextEmail.setText(signupEmail);
+
+            /**
+             * Clear signupEmail sharedPreferences to make sure that they are used just once
+             */
+            mSharedPrefEditor.putString(Constants.KEY_SIGNUP_EMAIL, null).apply();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthStateListener != null) {
+            mAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    public void onSignInPressed(View view) {
+        signInPassword();
+    }
+
+    private void initializeScreen() {
+        mEditTextEmail = (EditText) findViewById(R.id.edit_text_email);
+        mEditTextPassword = (EditText) findViewById(R.id.edit_text_password);
+        //LinearLayout linearLayoutLoginActivity = (LinearLayout) findViewById(R.id.linear_layout_login_activity);
+        //initializeBackground(linearLayoutLoginActivity);
+        /* Setup the progress dialog that is displayed later when authenticating with Firebase */
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle(getString(R.string.progress_dialog_loading));
+        mAuthProgressDialog.setMessage(getString(R.string.progress_dialog_authenticating_with_firebase));
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+    public void signInPassword() {
+        String email = mEditTextEmail.getText().toString();
+        String password = mEditTextPassword.getText().toString();
+
+        /**
+         * If email and password are not empty show progress dialog and try to authenticate
+         */
+        if (email.equals("")) {
+            mEditTextEmail.setError(getString(R.string.error_cannot_be_empty));
+            return;
+        }
+
+        if (password.equals("")) {
+            mEditTextPassword.setError(getString(R.string.error_cannot_be_empty));
+            return;
+        }
+        mAuthProgressDialog.show();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(LOG_TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(LOG_TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            /* Save provider name and encodedEmail for later use and start MainActivity */
+                            //mSharedPrefEditor.putString(Constants.KEY_PROVIDER, authData.getProvider()).apply();
+                            //mSharedPrefEditor.putString(Constants.KEY_ENCODED_EMAIL, mEncodedEmail).apply();
+
+                            /* Go to main activity */
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                        }
+
+
+                    }
+                });
+    }
+
+    protected void initializeBackground(LinearLayout linearLayout) {
+
+        /**
+         * Set different background image for landscape and portrait layouts
+         */
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            linearLayout.setBackgroundResource(R.drawable.background_loginscreen_land);
+        } else {
+            linearLayout.setBackgroundResource(R.drawable.background_loginscreen);
+        }
+    }
+}
